154123ab1f9ac117d94ed6d6e97e585e9f0dae24
Pawan Dubey <dpawan494@gmail.com>
2022-07-19 12:34:50 +0530

    Fix papa message

--- fundaddy/views.py
+++ fundaddy/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 
 
 def test_kids(request):
-    html = "<html><body>Congratulations for accepting <b>Pawan Dubey</b> as you papa.</body></html>"
+    html = "<html><body>Congratulations for accepting <b>Pawan Dubey</b> as your papa.</body></html>"
     return HttpResponse(html)
